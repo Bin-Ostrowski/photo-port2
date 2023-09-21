6f481871e7956b7d28716383683ff61265b1b44a
Robin Ostrowski <104393956+Bin-Ostrowski@users.noreply.github.com>
2023-09-21 15:34:19 -0700

    add about components tests

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import About from "..";
+
+//This will ensure that after each test,
+// we won't have any leftover memory data that could give us false results.
+afterEach(cleanup);
+
+describe("About component", () => {
+  //Frist Test
+  //'test' can also be used interchangeably with 'it' to create a test.
+  it("renders", () => {
+    render(<About></About>);
+  });
+
+  //Second Test
+  it("matches snapshot DOM node structure", () => {
+    // render About
+    const { asFragment } = render(<About />);
+    //Use the expect function with a matcher to assert something about a value.
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+//second test notes
+// IMPORTANT
+// When changes are made to a component's element, such as a change in text content,
+// button label, or attribute, it will cause the stored snapshot to become stale.
+// A new snapshot can be written at the command line that contains the instance
+// of the test runner by typing u to update, or rewrite, a new snapshot.
